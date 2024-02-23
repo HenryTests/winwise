838e3eed2a0f55bd73304f99547ea914db1d9b66
Enrique <enriquemarquezramirez@gmail.com>
2024-02-22 18:35:01 -0600

    import function response

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import {fetchChampionsData, fetchChampionsDataExtended} from "@/lib/api";
 
 export default async function Home() {
-  fetchChampionsDataExtended();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <p>Hola mundo</p>
     </main>
   );
 }
